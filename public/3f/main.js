01afa1be033abaa78401528976438b0088d02209
Mandy-PC\Mandy <mtwmt@hotmail.com>
2019-02-14 00:44:45 +0800

    no message

--- public/3f/main.js
+++ public/3f/main.js
@@ -1,3 +1,17 @@
 "use strict";
 
-(function () {})();
+(function () {
+  var $hd = document.querySelector('.cal-header'),
+      $hdcal = document.querySelector('.cal'),
+      $hdtotal = document.querySelector('.total'),
+      $num = document.querySelectorAll('.num'),
+      $sum = document.querySelector('.sum'),
+      hd_w = $hd.clientWidth - 40,
+      hdtotal_w = $hdtotal.clientWidth;
+  console.log(hd_w, $hdtotal.offsetWidth);
+
+  function textSize() {}
+
+  ;
+  textSize();
+})();
--- sourse/3f/main.js
+++ sourse/3f/main.js
@@ -1,3 +1,21 @@
 (function() {
-  
+  let $hd = document.querySelector('.cal-header'),
+      $hdcal = document.querySelector('.cal'),
+      $hdtotal = document.querySelector('.total'),
+      $num = document.querySelectorAll('.num'),
+      $sum = document.querySelector('.sum'),
+      hd_w = $hd.clientWidth - 40,
+      hdtotal_w = $hdtotal.clientWidth;
+
+      console.log(hd_w, $hdtotal.offsetWidth)
+
+      function textSize(){
+        
+
+      };
+      
+
+      textSize();
+
+
 })();
