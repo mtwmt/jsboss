9d00693590b335d5b603ff253f05cef80710c600
Mandy-PC\Mandy <mtwmt@hotmail.com>
2019-02-16 20:50:04 +0800

    no message

---
+++ public/6f/main.js
@@ -0,0 +1,166 @@
+"use strict";
+
+Vue.component('start', {
+  props: ['challenge'],
+  data: function data() {
+    return {};
+  },
+  methods: {
+    go: function go() {
+      this.$emit('getpage', 'play');
+    }
+  },
+  template: "\n  <section class=\"start\">\n    <div class=\"header\">\n      <div class=\"sec\">{{ challenge }}</div>\n      <div class=\"caption\">\n        <p>SECONDS</p>\n        <p>CHALLENGE</p>\n      </div>\n    </div>\n    <div class=\"main\">\n      <button\n        @click=\"go()\"\n      >START!</button>\n      <p class=\"tip\">try to answer more as you can</p>\n    </div>\n  </section>\n  "
+});
+Vue.component('play', {
+  props: ['challenge'],
+  data: function data() {
+    return {
+      score: 0,
+      result: '',
+      status: {
+        num1: '',
+        num2: '',
+        func: '',
+        sum: ''
+      },
+      time: ''
+    };
+  },
+  mounted: function mounted() {},
+  created: function created() {
+    this.time = this.challenge;
+    this.countdown();
+    this.game();
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    countdown: function countdown() {
+      var _self = this,
+          t = void 0;
+
+      if (--_self.time === 0) {
+        clearTimeout(t); // 雙向綁定
+
+        this.$emit('getscore', this.score);
+        this.$emit('getpage', 'end');
+      } else {
+        t = setTimeout(this.countdown, 1000);
+      } // if( _self.time === 0 ){
+      //   clearTimeout(t);
+      //   console.log( 'countdown' )
+      // }else{
+      //   _self.time--;
+      //   if( t ){
+      //     clearTimeout(t);
+      //   }
+      //   t = setTimeout( this.countdown, 1000 );
+      // }
+
+    },
+    // 亂數取值
+    randTime: function randTime(min, max) {
+      return Math.round(Math.random() * (max - min) + min);
+    },
+    isNum: function isNum() {
+      if (this.time > 40) {
+        return this.randTime(0, 9);
+      } else if (this.time > 20) {
+        return this.randTime(10, 99);
+      } else {
+        return this.randTime(100, 999);
+      }
+    },
+    isFunc: function isFunc() {
+      var func = ['+', '−', '×', '÷'];
+      return func[this.randTime(0, func.length - 1)];
+    },
+    game: function game() {
+      this.status.num1 = this.isNum();
+      this.status.num2 = this.isNum();
+      this.status.func = this.isFunc();
+
+      var _self = this,
+          sum = function sum(num1, num2, func) {
+        var isResult = void 0;
+
+        if (func === '+') {
+          isResult = num1 + num2;
+        } else if (func === '−') {
+          isResult = num1 - num2;
+        } else if (func === '×') {
+          isResult = num1 * num2;
+        } else if (func === '÷') {
+          isResult = num1 / num2;
+        }
+
+        if (isResult.toString().indexOf('.') == 1 || isResult == 'Infinity' || isResult < 0) {
+          return _self.game();
+        }
+
+        console.log('isResult', isResult);
+        return _self.status.sum = isResult;
+      };
+
+      sum(_self.status.num1, _self.status.num2, _self.status.func);
+    },
+    answer: function answer() {
+      if (this.time > 20 || this.status.num1.toString().length <= 2) {
+        if (this.result == this.status.sum) {
+          this.score += 1;
+        }
+      } else {
+        if (this.result == this.status.sum) {
+          this.score += 5;
+        } else {
+          this.score -= 1;
+
+          if (this.score <= 0) {
+            this.score = 0;
+          }
+        }
+      }
+
+      this.game();
+      this.result = '';
+    }
+  },
+  template: "\n  <section class=\"play\">\n    <div class=\"header\">\n      <div class=\"now\">\n        <label>{{ challenge }} SECONDS CHALLENGE</label>\n        <p><span>SCORE</span>{{ score }}</p>\n      </div>\n      <div class=\"time\">{{ time }}</div>\n    </div>\n    <div class=\"main\">\n      <div class=\"calculate\">\n        <span class=\"num\">{{ status.num1 }}</span>\n        <span class=\"func\">{{ status.func }}</span>\n        <span class=\"num\">{{ status.num2 }}</span>\n        <span class=\"func\">=</span>\n        <span class=\"sum\" >\n          <input type=\"text\" \n          v-model=\"result\" \n          @keyup.enter=\"answer()\"\n          />\n        </span>\n      </div>\n    </div>\n  </section>\n  "
+});
+Vue.component('end', {
+  props: ['challenge', 'page', 'score'],
+  data: function data() {
+    return {};
+  },
+  methods: {
+    go: function go() {
+      this.$emit('getscore', 0);
+      this.$emit('getpage', 'start');
+    }
+  },
+  template: "\n  <section class=\"end\">\n    <div class=\"main\">\n      <label>{{ challenge }} SECONDS CHALLENGE</label>\n      <p class=\"final\">YOUR FINAL SCORE</p>\n      <p class=\"result\">{{ score }}</p>\n      <button\n       @click=\"go()\"\n      >TRY AGAIN!</button>\n    </div>\n  </section>\n  "
+});
+var vm = new Vue({
+  el: '#app',
+  data: {
+    page: "start",
+    challenge: 60,
+    score: 0,
+    time: ''
+  },
+  created: function created() {},
+  computed: {},
+  methods: {
+    getscore: function getscore(data) {
+      this.score = data;
+    },
+    gettime: function gettime(data) {
+      this.time = data || this.challenge;
+    },
+    getpage: function getpage(data) {
+      this.page = data;
+    }
+  },
+  template: "\n  <div>\n    <start \n      v-if=\"page === 'start'\"\n      :challenge=\"challenge\"\n      @getpage=\"getpage\"\n    />\n    <play \n      v-else-if=\"page === 'play'\"\n      :challenge=\"challenge\"\n      @getscore=\"getscore\"\n      @getpage=\"getpage\"\n    />\n    <end \n      v-else=\"page === 'end'\"\n      :challenge=\"challenge\"\n      :score=\"score\"\n      @getscore=\"getscore\"\n      @getpage=\"getpage\"\n    />\n  </div>\n  "
+});
---
+++ sourse/6f/main.js
@@ -0,0 +1,236 @@
+Vue.component('start',{
+  props: ['challenge'],
+  data: function(){
+    return{}
+  },
+  methods: {
+    go(){
+      this.$emit('getpage','play'); 
+    }
+  },
+  template: `
+  <section class="start">
+    <div class="header">
+      <div class="sec">{{ challenge }}</div>
+      <div class="caption">
+        <p>SECONDS</p>
+        <p>CHALLENGE</p>
+      </div>
+    </div>
+    <div class="main">
+      <button
+        @click="go()"
+      >START!</button>
+      <p class="tip">try to answer more as you can</p>
+    </div>
+  </section>
+  `
+});
+
+Vue.component('play',{
+  props: ['challenge'],
+  data: function(){
+    return{
+      score: 0,
+      result: '',
+      status: {
+        num1: '',
+        num2: '',
+        func: '',
+        sum: ''
+      },
+      time: '',
+    }
+  },
+  mounted(){},
+  created() {
+    this.time = this.challenge;
+    this.countdown();
+    this.game();
+  },
+  watch:{},
+  computed: {},
+  methods: {
+    countdown(){
+      let _self = this,
+            t;
+      if( --_self.time === 0 ){
+        clearTimeout(t);
+        // 雙向綁定
+        this.$emit('getscore', this.score);
+        this.$emit('getpage', 'end');
+      }else{
+        t = setTimeout( this.countdown, 1000 );
+      }
+      // if( _self.time === 0 ){
+      //   clearTimeout(t);
+      //   console.log( 'countdown' )
+      // }else{
+      //   _self.time--;
+      //   if( t ){
+      //     clearTimeout(t);
+      //   }
+      //   t = setTimeout( this.countdown, 1000 );
+      // }
+    },
+    // 亂數取值
+    randTime(min, max) {
+      return Math.round(Math.random() * (max - min) + min);
+    },
+    isNum(){
+      if( this.time > 40 ){
+        return this.randTime( 0,9 );
+      }else if( this.time > 20 ){
+        return this.randTime( 10,99 );
+      }else{
+        return this.randTime( 100,999 );
+      }
+    },
+    isFunc(){
+      const func = ['+','−','×','÷'];
+      return func[this.randTime( 0, func.length - 1 )];
+    },
+    game(){
+      this.status.num1 = this.isNum();
+      this.status.num2 = this.isNum();
+      this.status.func = this.isFunc();
+
+      let _self = this,
+          sum = function( num1, num2, func ){
+            let isResult;
+            if (func === '+') {
+              isResult = num1 + num2;
+            } else if (func === '−') {
+              isResult = num1 - num2;
+            } else if (func === '×') {
+              isResult = num1 * num2;
+            }else if (func === '÷') {
+              isResult = num1 / num2;
+            }
+            if( isResult.toString().indexOf('.') == 1 || isResult == 'Infinity' || isResult < 0 ){
+              return _self.game()
+            }
+            console.log( 'isResult',isResult )
+            return _self.status.sum = isResult;
+          };
+      sum( _self.status.num1, _self.status.num2, _self.status.func);
+    },
+    answer(){
+      if( this.time > 20 || this.status.num1.toString().length <= 2 ){
+        if( this.result == this.status.sum ){
+          this.score += 1;
+        }
+      }else{
+        if( this.result == this.status.sum ){
+          this.score += 5;
+        }else{
+          this.score -= 1;  
+          if( this.score <= 0 ){
+            this.score = 0;  
+          }
+        }
+      }
+      this.game();
+      this.result = '';
+    }
+  },
+  template: `
+  <section class="play">
+    <div class="header">
+      <div class="now">
+        <label>{{ challenge }} SECONDS CHALLENGE</label>
+        <p><span>SCORE</span>{{ score }}</p>
+      </div>
+      <div class="time">{{ time }}</div>
+    </div>
+    <div class="main">
+      <div class="calculate">
+        <span class="num">{{ status.num1 }}</span>
+        <span class="func">{{ status.func }}</span>
+        <span class="num">{{ status.num2 }}</span>
+        <span class="func">=</span>
+        <span class="sum" >
+          <input type="text" 
+          v-model="result" 
+          @keyup.enter="answer()"
+          />
+        </span>
+      </div>
+    </div>
+  </section>
+  `
+});
+
+Vue.component('end',{
+  props: ['challenge','page','score'],
+  data: function(){
+    return{}
+  },
+  methods: {
+    go(){
+      this.$emit('getscore', 0)
+      this.$emit('getpage', 'start');
+    }
+  },
+  template: `
+  <section class="end">
+    <div class="main">
+      <label>{{ challenge }} SECONDS CHALLENGE</label>
+      <p class="final">YOUR FINAL SCORE</p>
+      <p class="result">{{ score }}</p>
+      <button
+       @click="go()"
+      >TRY AGAIN!</button>
+    </div>
+  </section>
+  `
+});
+
+var vm = new Vue({
+  el: '#app',
+  data:{
+    page: "start",
+    challenge: 60,
+    score: 0,
+    time:'',
+  },
+  created() {},
+  computed: {
+  },
+  methods: {
+    getscore( data ){
+      this.score = data;
+    },
+    gettime (data){
+      this.time = data || this.challenge;
+    },
+    getpage( data ){
+      this.page = data;
+    }
+  },
+  template: `
+  <div>
+    <start 
+      v-if="page === 'start'"
+      :challenge="challenge"
+      @getpage="getpage"
+    />
+    <play 
+      v-else-if="page === 'play'"
+      :challenge="challenge"
+      @getscore="getscore"
+      @getpage="getpage"
+    />
+    <end 
+      v-else="page === 'end'"
+      :challenge="challenge"
+      :score="score"
+      @getscore="getscore"
+      @getpage="getpage"
+    />
+  </div>
+  `
+});
+
+
+ 
