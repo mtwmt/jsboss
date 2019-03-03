fd23fdaba9a0842f863387dae47ee5627daa2c12
Mandy-PC\Mandy <mtwmt@hotmail.com>
2019-03-04 02:20:57 +0800

    no message

--- public/8f/main.js
+++ public/8f/main.js
@@ -1,9 +1,77 @@
 "use strict";
 
+Vue.component('start', {
+  props: [],
+  methods: {
+    gopage: function gopage() {
+      this.$emit('getpage', 'game');
+    }
+  },
+  template: "\n  <div class=\"start\">\n    <div class=\"tictactoe\">\n      <div>TIC</div>\n      <div>\u25CB</div>\n      <div>\u2716</div>\n      <div>\u2716</div>\n      <div>TAC</div>\n      <div>\u25CB</div>\n      <div>\u25CB</div>\n      <div>\u2716</div>\n      <div>TOC</div>\n      <button @click=\"gopage\">START</button>\n    </div>\n  </div>\n  "
+});
+Vue.component('game', {
+  props: [],
+  data: function data() {
+    return {
+      step: 0,
+      score: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      cal: [] // score: [],
+
+    };
+  },
+  watch: {
+    step: function step() {
+      var _this = this;
+
+      this.score.map(function (e, idx) {
+        if (e !== 0) {
+          _this.cal = [_this.score[1] + _this.score[2] + _this.score[3], _this.score[4] + _this.score[5] + _this.score[6], _this.score[7] + _this.score[8] + _this.score[9]];
+        }
+      });
+    }
+  },
+  computed: {// eval(){
+    //   return [
+    //     this.score[1] + this.score[2] + this.score[3],
+    //     this.score[4] + this.score[5] + this.score[6],
+    //   ]
+    // }
+  },
+  methods: {
+    kick: function kick(key) {
+      var _this2 = this;
+
+      this.step += 1;
+
+      var _self = this,
+          $tictactoe = _self.$el.querySelector('.tictactoe'),
+          $panel = $tictactoe.querySelectorAll('div');
+
+      $panel.forEach(function (el, idx) {
+        if (idx + 1 === key && !el.querySelector('i')) {
+          if (_this2.step % 2 === 1) {
+            el.innerHTML = '<i class="far fa-circle"></i>';
+            _this2.score[key] = 1;
+          } else {
+            el.innerHTML = '<i class="fas fa-times"></i>';
+            _this2.score[key] = -1;
+          }
+        }
+      });
+    }
+  },
+  template: "\n  <div class=\"game\">\n    <div class=\"tictactoe\">\n      <div \n        v-for=\"i in 9\" \n        :key=\"i\"\n        :data-num=\"i\"\n        @click=\"kick(i)\"\n      ></div>\n      <button>RESTART</button>\n    </div>\n  </div>\n  "
+});
 var vm = new Vue({
   el: '#app',
-  data: {} // template: `
-  //   <div>123</div>
-  // `
-
+  data: {
+    page: 'game',
+    winner: ''
+  },
+  methods: {
+    getpage: function getpage(data) {
+      this.page = data;
+    }
+  },
+  template: "\n  <div class=\"main\">\n    <start \n      v-if=\"page === 'start'\"\n      @getpage=\"getpage\"\n    />\n    <game \n      v-else-if=\"page === 'game'\"\n      @getpage=\"getpage\"\n    />\n    <winner\n      v-else=\"page === 'winner'\"\n      @getpage=\"getpage\"\n    />\n    <end\n      v-else=\"page === 'end'\"\n      @getpage=\"getpage\"\n    />\n  </div>\n  "
 });
--- sourse/8f/main.js
+++ sourse/8f/main.js
@@ -1,9 +1,126 @@
+Vue.component('start',{
+  props:[],
+  methods:{
+    gopage(){
+      this.$emit('getpage','game'); 
+    }
+  },
+  template: `
+  <div class="start">
+    <div class="tictactoe">
+      <div>TIC</div>
+      <div>○</div>
+      <div>✖</div>
+      <div>✖</div>
+      <div>TAC</div>
+      <div>○</div>
+      <div>○</div>
+      <div>✖</div>
+      <div>TOC</div>
+      <button @click="gopage">START</button>
+    </div>
+  </div>
+  `
+});
+
+Vue.component('game',{
+  props:[],
+  data(){
+    return {
+      step: 0,
+      score: [0,0,0,0,0,0,0,0,0],
+      cal: [],
+      // score: [],
+    }
+  },
+  watch:{
+    step(){
+
+      this.score.map( (e,idx)=>{
+        if( e !== 0 ){
+          this.cal = [
+            this.score[1] + this.score[2] + this.score[3],
+            this.score[4] + this.score[5] + this.score[6],
+            this.score[7] + this.score[8] + this.score[9],
+          ]
+        }
+      })
+    },
+  },
+  computed:{
+    // eval(){
+    //   return [
+    //     this.score[1] + this.score[2] + this.score[3],
+    //     this.score[4] + this.score[5] + this.score[6],
+    //   ]
+    // }
+  },
+  methods:{
+    kick( key ){
+      this.step += 1;
+      let _self = this,
+          $tictactoe = _self.$el.querySelector('.tictactoe'),
+          $panel = $tictactoe.querySelectorAll('div');
+
+      $panel.forEach( (el,idx) => {
+        if( idx+1 === key && !el.querySelector('i') ){
+          if( (this.step % 2) === 1){
+            el.innerHTML = '<i class="far fa-circle"></i>';
+            this.score[key] = 1;
+          }else{
+            el.innerHTML = '<i class="fas fa-times"></i>';
+            this.score[key] = -1;
+          } 
+        }
+      });
+      
+    }
+  },
+  template:`
+  <div class="game">
+    <div class="tictactoe">
+      <div 
+        v-for="i in 9" 
+        :key="i"
+        :data-num="i"
+        @click="kick(i)"
+      ></div>
+      <button>RESTART</button>
+    </div>
+  </div>
+  `
+});
+
+
 var vm = new Vue({
   el: '#app',
   data:{
-
+    page: 'game',
+    winner: '',
   },
-  // template: `
-  //   <div>123</div>
-  // `
+  methods: {
+    getpage( data ){
+      this.page = data
+    }
+  },
+  template: `
+  <div class="main">
+    <start 
+      v-if="page === 'start'"
+      @getpage="getpage"
+    />
+    <game 
+      v-else-if="page === 'game'"
+      @getpage="getpage"
+    />
+    <winner
+      v-else="page === 'winner'"
+      @getpage="getpage"
+    />
+    <end
+      v-else="page === 'end'"
+      @getpage="getpage"
+    />
+  </div>
+  `
 });
