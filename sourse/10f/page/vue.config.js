48dd44aae39f6e980156ed8f1cf623f794c247ab
Mandy <g1700@evertrust.com.tw>
2019-03-21 19:06:23 +0800

    no message

--- sourse/10f/page/vue.config.js
+++ sourse/10f/page/vue.config.js
@@ -12,9 +12,24 @@
 //   }
 // }
 
+// var modeEnv = process.argv[3].split("=")[1];
+
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
   chainWebpack: config => {
     config.module.rules.delete('eslint');
+  },
+  pages: {
+    newtab: {
+      entry: 'src/newtab/main.js',
+      template: 'public/index.html',
+      filename: 'newtab.html'
+    },
+    popup: {
+      entry: 'src/popup/main.js',
+      template: 'public/index.html',
+      filename: 'popup.html'
+    }
   },
 };
